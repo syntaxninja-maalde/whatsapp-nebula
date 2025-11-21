15882ee052aae5cc72eb40883a020824c7a713f0
unknown <syntaxninja.maalde@gmail.com>
2025-11-21 17:34:59 +0530

    Fauzan init

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,10 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 
-export default config;
+/** @type {import('postcss-load-config').Config} */
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+};
